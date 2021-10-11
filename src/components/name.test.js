1c73273f7c2a26920751f8796d41f9fb0c226c5f
mrc-laufire <mrc@laufire.in>
2021-10-11 08:58:06 +0000

    Tests abstracted.

--- src/components/name.test.js
+++ src/components/name.test.js
@@ -1,34 +1,9 @@
-import { render, fireEvent } from '@testing-library/react';
 import TestHelpers from '../../test/helpers';
 import Name from './name';
 
-describe('Name', () => {
-	const { rndString } = TestHelpers;
-	const name = rndString();
-	const context = {
-		state: {
-			name,
-		},
-		actions: {
-			patchState: jest.fn(),
-		},
-	};
-
-	test('Renders the component', () => {
-		const component = render(Name(context)).getByRole('name');
-
-		expect(component).toBeInTheDocument();
-		expect(component).toHaveClass('name');
-		expect(component.value).toEqual(name);
-		expect(component.type).toEqual('text');
-	});
-
-	test('triggers action which patches the state', () => {
-		const value = rndString();
-		const component = render(Name(context)).getByRole('name');
-
-		fireEvent.change(component, { target: { value }});
-
-		expect(context.actions.patchState).toBeCalledWith({ name: value });
-	});
+TestHelpers.testInput({
+	component: Name,
+	name: 'name',
+	type: 'text',
+	value: TestHelpers.rndString(),
 });
--- test/helpers.js
+++ test/helpers.js
@@ -1,10 +1,45 @@
+/* eslint-disable max-lines-per-function */
 import { rndString } from '@laufire/utils/random';
+import { render, fireEvent } from '@testing-library/react';
 
 const TestHelpers = {
 	rndString: () => {
 		const idLength = 8;
 
 		return rndString(idLength);
+	},
+	testInput: ({ component, name, type, value }) => {
+		describe(component, () => {
+			const context = {
+				state: {
+				},
+				actions: {
+					patchState: jest.fn(),
+				},
+			};
+
+			context.state[name] = TestHelpers.rndString();
+
+			test('Renders the component', () => {
+				const rendered = render(component(context))
+					.getByRole(name);
+
+				expect(rendered).toBeInTheDocument();
+				expect(rendered).toHaveClass(name);
+				expect(rendered.value).toEqual(context.state[name]);
+				expect(rendered.type).toEqual(type);
+			});
+
+			test('triggers action which patches the state', () => {
+				const rendered = render(component(context))
+					.getByRole(name);
+
+				fireEvent.change(rendered, { target: { value }});
+
+				expect(context.actions.patchState)
+					.toHaveBeenCalledWith({ [name]: value });
+			});
+		});
 	},
 };
 
